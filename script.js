ec1c1e7f66f9e80d59d4726797a97d40c2d69262
ashishsaini <ashy.saini@gmail.com>
2021-03-12 19:11:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
 function showTime(){
     var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+    var h = addZero(date.getHours()); // 0 - 23
+    var m = addZero(date.getMinutes()); // 0 - 59
+    var s = addZero(date.getSeconds()); // 0 - 59
         
+    
     var time = h + ":" + m ;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
-    setTimeout(showTime, 10000);
+    setTimeout(showTime, 60000);
+    location.reload();
     
 }
 
