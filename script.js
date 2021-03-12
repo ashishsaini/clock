b9588ebd146478b78995b14650bb1f17c1c83a6f
ashishsaini <ashy.saini@gmail.com>
2021-03-12 19:31:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function addZero(i) {
 }
 
 const interval = setInterval(function() {
-    var date = new Date();
+    var currentTime = new Date();
+    var currentOffset = currentTime.getTimezoneOffset();
+    var ISTOffset = 330;   // IST offset UTC +5:30 
+    var date = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+    //var date = new Date();
     var h = addZero(date.getHours()); // 0 - 23
     var m = addZero(date.getMinutes()); // 0 - 59
     var s = addZero(date.getSeconds()); // 0 - 59
